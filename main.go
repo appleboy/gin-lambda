9c35c773e20d2aec078c62d106bd93a5bffc369e
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-01-20 10:47:23 +0800

    feat: add gin framework

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/apex/gateway"
+	"github.com/gin-gonic/gin"
 )
+
+func welcomeHandler(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World from Go")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"text": "Welcome to gin lambda server.",
+	})
+}
+
+func routerEngine() *gin.Engine {
+	// set server mode
+	gin.SetMode(gin.DebugMode)
+
+	r := gin.New()
+
+	// Global middleware
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.GET("/welcome", welcomeHandler)
+	r.GET("/", rootHandler)
+
+	return r
+}
 
 func main() {
 	addr := ":" + os.Getenv("PORT")
-	http.HandleFunc("/", hello)
-	log.Fatal(gateway.ListenAndServe(addr, nil))
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World from Go")
+	log.Fatal(gateway.ListenAndServe(addr, routerEngine()))
 }
